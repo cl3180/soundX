10bab62c121c969aeeb760bada299c3ab4541a6f
Cherie Luo <cherieluo@Cheries-MacBook-Air.local>
2015-01-02 10:55:06 -0500

    Initial Commit

---
+++ Sounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sounds
+//
+//  Created by Cherie Luo on 1/2/15.
+//  Copyright (c) 2015 Cherie Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
